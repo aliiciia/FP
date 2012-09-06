eb281fc29297d87b395b366bf83ffadde25b7dc4
Alicia Deshield <adeshield@fullsail.edu>
2012-09-06 18:35:31 -0400

    rearranged files to add fuelphp

---
+++ _static/js/main.js
@@ -0,0 +1,70 @@
+
+$(document).ready(function(){
+    $('ul.tabs').each(function(){
+	var $active, $content, $links = $(this).find('a');
+	
+				    
+	$active = $($links.filter('[href="'+location.hash+'"]')[0] || $links[0]);
+	$active.addClass('active');
+	$content = $($active.attr('href'));
+	
+	
+	$links.not($active).each(function () {
+	    $($(this).attr('href')).hide();
+	});
+	
+				    
+	$(this).on('click', 'a', function(e){
+					    
+	    $active.removeClass('active');
+	    $content.hide();
+	    
+						
+	    $active = $(this);
+	    $content = $($(this).attr('href'));
+	    
+						
+	    $active.addClass('active');
+	    $content.show();
+	    
+						
+	    e.preventDefault();
+	});
+    });
+});
+
+$(document).ready(function($) {
+      $('#photo a').smoothScroll({
+        speed: 1000,
+        easing: 'easeInOutCubic'
+      });
+
+      $('.showOlderChanges').on('click', function(e){
+        $('.changelog .old').slideDown('slow');
+        $(this).fadeOut();
+        e.preventDefault();
+      })
+  });
+
+
+ $(function () {
+    $(".youtube").YouTubePopup({ autoplay: 0 });
+    $(".youtube").YouTubePopup({ hideTitleBar: true });
+});
+ 
+$(function(){
+      SyntaxHighlighter.all();
+});
+
+    $(window).load(function(){
+	$('.flexslider').flexslider({
+        animation: "slide",
+	    start: function(slider){
+	    $('body').removeClass('loading');
+	    }
+    });
+});
+    
+
+  
+   
