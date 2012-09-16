1313376b26ba7aaf1c317d2cef21edb98f881a3b
Alicia Deshield <adeshield@fullsail.edu>
2012-09-16 19:00:55 -0400

    production

---
+++ fuel/app/logs/2012/09/16.php
@@ -0,0 +1,4 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+Error - 2012-09-16 21:43:14 --> 8 - Undefined variable: title in /Users/adeshield/Sites/mmz/fuel/app/views/error/404.php on line 5
+Error - 2012-09-16 21:43:14 --> 8 - Undefined variable: title in /Users/adeshield/Sites/mmz/fuel/app/views/error/404.php on line 45
