337eea0d7e7364189d125be314f0bd2595154a8b
Alicia Deshield <adeshield@fullsail.edu>
2012-09-20 03:42:26 -0400

    copy page and jquery

---
+++ fuel/app/views/site/copy.php
@@ -0,0 +1,23 @@
+<div class="copyright"> 
+
+<h1>Legal &amp; Copyright</h1>
+
+
+<h3>Trademark</h3>
+
+<p>All written, graphical, video, interactive and multimedia content is owned
+by The MegaMindz.</p>
+
+<h3>Copyright Notice</h3>
+<p>In no event may this information be reproduced, distributed
+and/or publicly displayed in any form or by any means without prior
+expressed written permission of The MegaMindz.</p>
+
+<h3>General</h3>
+<p> You acknowledge and agree that these Terms and Conditions, together with our Privacy Statement, constitute the complete and exclusive agreement between us concerning your use of the website, and supersede and govern all prior proposals, agreements, or other communications.
+
+We reserve the right, in our sole discretion, to change these Terms and Conditions at any time by posting the changes on the website. Any changes are effective immediately upon posting to the website. Your continued use of the website constitutes your agreement to all such terms and conditions. We may, with or without prior notice, terminate any of the rights granted by these Terms and Conditions. You shall comply immediately with any termination or other notice, including, as applicable, by ceasing all use of the website.
+
+Nothing contained in these Terms and Conditions shall be construed as creating any agency, partnership, or other form of joint enterprise between us. Our failure to require your performance of any provision hereof shall not affect the full right to require such performance at any time thereafter, nor shall our waiver of a breach of any provision hereof be taken or held to be a waiver of the provision itself. In the event that any provision of these Terms and Conditions shall be unenforceable or invalid under any applicable law or be so held by any applicable court decision, such unenforceability or invalidity shall not render these Terms and Conditions unenforceable or invalid as a whole. We will amend or replace such provision with one that is valid and enforceable and which achieves, to the extent possible, our original objectives and intent as reflected in the original provision.</p>
+
+</div>
--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -86,6 +86,17 @@ $(document).ready(function(){
             errorText: '{label} is a required field.'
         });   
 
+       var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-34963602-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+
 }); //end document ready
 
 })(jQuery); //end private scope
